55fb14341ba9ff9e803ecf3360a2ba4703c9969f
jacktang <jack at zzfs>
2017-08-11 16:44:09 +0800

    1

--- luatest.c
+++ luatest.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+lua_State *global_lua;
 
 int main(int argc, char *argv[])
 {
     
     return 0;
 }
+
+
+void scriptingInit(int setup) {
+    lua_State *lua = lua_open();
+
+	if (setup)
+	{
+	}
+
+//    luaLoadLibraries(lua);
+//    luaRemoveUnsupportedFunctions(lua);
+
+    /* Lua beginners often don't use "local", this is likely to introduce
+     * subtle bugs in their code. To prevent problems we protect accesses
+     * to global variables. */
+//    scriptingEnableGlobalsProtection(lua);
+
+	global_lua = lua;
+}
+
+/* Release resources related to Lua scripting.
+ * This function is used in order to reset the scripting environment. */
+void scriptingRelease(void) {
+    lua_close(global_lua);
+}
+
+void scriptingReset(void) {
+    scriptingRelease();
+    scriptingInit(0);
+}
+
+void luaSetGlobalArray(lua_State *lua, char *var, char **elev, int elec) {
+    int j;
+
+    lua_newtable(lua);
+    for (j = 0; j < elec; j++) {
+        lua_pushlstring(lua, elev[j],strlen(elev[j]));
+        lua_rawseti(lua,-2,j+1);
+    }
+    lua_setglobal(lua,var);
+}
+
+
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 CC = gcc
 CFLAGS = -g -O0 -Wall
+LUA_VERSION = 5.1
 luatest: luatest.c
-	$(CC) $(CFLAGS) $^ -o $@ 
+	$(CC) $(CFLAGS) -I/usr/include/lua$(LUA_VERSION) $^ -llua$(LUA_VERSION) -o $@ 
